fb3becc796d92e33ee17673daf9249656b0cb1a2
Peter Potrok <peter.potrok@outlook.sk>
2025-04-05 07:39:57 +0200

    Add files via upload

---
+++ Math.h
@@ -0,0 +1,185 @@
+/******************************************************************************
+* \file      Math.h
+* \version   2025.03.18.01
+* \author    Peter Potrok
+* \copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* \brief     Math macros
+* \details
+*
+* \see       
+*            and other resources
+******************************************************************************/
+#ifndef MATH_H
+#define MATH_H		20250318L
+
+
+/******************************************************************************
+* \brief
+*	Check if the number is odd.
+*
+* \param
+*	n - Number to check.
+*
+* \return
+*	true, false.
+*
+*/
+#define IS_ODD(n)               ( (n) & 1 )
+
+/******************************************************************************
+* \brief
+*	Check if the number is even.
+*
+* \param
+*	n - Number to check.
+*
+* \return
+*	true, false.
+*
+*/
+#define IS_EVEN(n)              ( !IS_ODD(n) )
+
+
+/******************************************************************************
+* \brief
+*	Check if the number is between minimum and maximum.
+*
+* \param
+*	n - Number to check.
+* \param
+*	min - Minimum.
+* \param
+*	max - Maximum.
+*
+* \return
+*	true, false.
+*
+*/
+#define IS_BETWEEN(n, min, max)	(((n) >= (min)) && ((n) <= (max)))
+
+
+/******************************************************************************
+* \brief
+*	Inserts the value of a number into an interval based on the minimum and maximum.
+*
+* \param
+*	n - Number.
+* \param
+*	min - Minimum.
+* \param
+*	max - Maximum.
+*
+* \return
+*	New number value.
+*
+*/
+#define CLIP(n, min, max)		(((n) < (min)) ? (min) : ((n) > (max)) ? (max) : (n))
+
+/******************************************************************************
+* \brief
+*	Inserts the value of a number into an interval based on the maximum.
+*
+* \param
+*	n - Number.
+* \param
+*	max - Maximum.
+*
+* \return
+*	New number value.
+*
+*/
+#define UCLIP(n, max)			(((n) > (max)) ? (max) : (n))
+
+/******************************************************************************
+* \brief
+*	Inserts the value of a number into an interval based on the minimum.
+*
+* \param
+*	n - Number.
+* \param
+*	min - Minimum.
+*
+* \return
+*	New number value.
+*
+*/
+#define LCLIP(n, min)			(((n) < (min)) ? (min) : (n))
+
+
+/******************************************************************************
+* \brief
+*	Return the sign of the number.
+*
+* \param
+*	n - Number.
+*
+* \return
+*	-1, 0, 1.
+*
+*/
+#define SIGN(n)                 ( (n) > 0 ? 1 : ( (n) < 0 ? -1 : 0 ) )
+
+
+/******************************************************************************
+* \brief
+*	Return the absolute value of the number.
+*
+* \param
+*	n - Number.
+*
+* \return
+*	Absolute value.
+*
+*/
+#define ABS(n)                  ( (n) > 0 ? (n) : (-(n)) )
+
+
+/******************************************************************************
+* \brief
+*	Return the difference of numbers.
+*
+* \param
+*	n1 - Number.
+* \param
+*	n2 - Number.
+*
+* \return
+*	Difference of numbers.
+*
+*/
+#define DIFF(n1, n2)            ABS((n1)-(n2))
+
+
+/******************************************************************************
+* \brief
+*	Return the minimum value of numbers.
+*
+* \param
+*	n1 - Number.
+* \param
+*	n2 - Number.
+*
+* \return
+*	Minimum value.
+*
+*/
+#define MIN(n1, n2)             ( (n1) < (n2) ? (n1) : (n2) )
+
+/******************************************************************************
+* \brief
+*	Return the maximum value of numbers.
+*
+* \param
+*	n1 - Number.
+* \param
+*	n2 - Number.
+*
+* \return
+*	Maximum value.
+*
+*/
+#define MAX(n1, n2)             ( (n1) > (n2) ? (n1) : (n2) )
+
+
+#endif /* MATH_H */
---
+++ MathAngle.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+* \file      MathAngle.h
+* \version   2025.03.18.01
+* \author    Peter Potrok
+* \copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* \brief     Math angle macros
+* \details
+*
+* \see
+*            and other resources
+******************************************************************************/
+#ifndef MATHANGLE_H
+#define MATHANGLE_H		20250318L
+
+
+/* precise pi */
+#define PI                      3.1415926535897932384626433832795
+
+
+#define DEGREE_TO_RADIAN(n)     ((n) * PI / 180.0)
+
+#define DEGREE_TO_GRADIAN(n)    (((n) * 400.0) / 360.0)
+
+#define RADIAN_TO_DEGREE(n)     ((n) / PI * 180.0)
+
+#define RADIAN_TO_GRADIAN(n)    ((n) * 63.661977)
+
+#define GRADIAN_TO_DEGREE(n)    ((n) * 0.9)
+
+#define GRADIAN_TO_RADIAN(n)    ((n) * 0.015708)
+
+
+#endif /* MATHANGLE_H */
